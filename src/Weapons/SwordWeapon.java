f9b3a7022c1a47f4c3d450ffae27a8a5acbf2e89
Bruno <brunoplus.95@gmail.com>
2018-01-31 19:51:29 -0600

    mod

---
+++ src/Weapons/SwordWeapon.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Weapons;
+
+import Behaviors.WeaponBehavior;
+
+/**
+ *
+ * @author User
+ */
+public class SwordWeapon implements WeaponBehavior{
+
+    @Override
+    public void use() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
